2c3d7ba835a14b10d8f188cc77b7e959d45e0b1f
Jorge Romao <jorgeromao83@gmail.com>
2018-01-28 16:18:26 -0500

    final models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
   has_many :customer_courses
-  has_many :courses, through: :customer_courses
-  has_many :instructors, through: :courses
+  has_many :courses,through: :customer_courses
+  has_many :instructors,through: :courses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Jorge = Instructor.create(username:"jlromao", last_name: "Romao", first_name: "Jorge", email: "jorge@gmail.com")
