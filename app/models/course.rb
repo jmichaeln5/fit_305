5af98df8e01b6033054f5b4fb283fcf3b1afc978
Jorge Romao <jorgeromao83@gmail.com>
2018-01-29 15:52:53 -0500

    Stripe API integrated & time since Course was created

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   belongs_to :instructor, dependent: :destroy
   has_many :customer_courses
   has_many :customers, through: :customer_courses
+  validates :name, presence: true
+  validates :description, presence: true
 end
