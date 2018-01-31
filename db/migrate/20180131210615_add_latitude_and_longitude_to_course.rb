db7c8727ccf071491cdd1aaf6c728b74d7f6de6f
Jon Norton <jmichaeln5@gmail.com>
2018-01-31 16:10:02 -0500

    installed gem geocoder, migrate longitude/latitude

---
+++ db/migrate/20180131210615_add_latitude_and_longitude_to_course.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToCourse < ActiveRecord::Migration[5.1]
+  def change
+    add_column :courses, :latitude, :float
+    add_column :courses, :longitude, :float
+  end
+end
